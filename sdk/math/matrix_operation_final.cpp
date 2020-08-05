1d5a00efd25133b58d516ee53f7867555dbd88ac
stormytrail <pannengchao@126.com>
2020-08-05 23:50:41 +0800

    pack

---
+++ sdk/math/matrix_operation_final.cpp
@@ -0,0 +1,23 @@
+#include "matrix_operation.h"
+
+void matrix_transpose(const float* src,size_t M,size_t N,float* dest){
+	for (size_t i = 0;i < M;i++){
+		for (size_t j = 0;j < N;j++){
+			dest[j * M + i] = src[i * N + j];
+		}
+	}
+	return;
+}
+
+void c_sgemm(size_t M,size_t N,size_t K,float alpha,float* A,size_t lda,float* B,size_t ldb,float beta,float* C,size_t ldc){
+	for (size_t i = 0;i < M;i++){
+		for (size_t j = 0;j < N;j++){
+			C[i * N + j] *= beta;
+			for (size_t k = 0;k < K;k++){
+				C[i * N + j] += A[i * K + k] * B[k * N + j];
+			}
+		}
+	}
+
+	return;
+}
